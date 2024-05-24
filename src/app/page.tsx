f89ef2e8eaaedf2061a2ade76774846ff8046c60
torippi <torii.ryuto.23@shizuoka.ac.jp>
2024-05-24 15:10:51 +0900

    チュートリアルおしまい

---
+++ src/app/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ForcastData {
+  publishingOffice: string;
+  reportDatetime: string;
+  targetArea: string;
+  headlineText: string;
+  text: string;
+}
+
+export default function Home() {
+  const area: String[] = ["山梨県", "長野県", "岐阜県", "静岡県", "愛知県"];
+  const code: String[] = ["190000", "200000", "210000", "220000", "230000"];
+  const [idx, setIdx] = useState<number>(0);
+  const inc = () => {
+    setIdx((idx + 1) % area.length);
+    console.log(idx);
+  }
+  const dec = () => {
+    setIdx((idx + area.length - 1) % area.length);
+    console.log(idx);
+  }
+
+  const [data, setData] = useState<ForcastData>();
+
+  useEffect(() => {
+    const apiUrl = `https://www.jma.go.jp/bosai/forecast/data/overview_forecast/${code[idx]}.json`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+  
+    fetchData();
+  }, [idx, code]);
+
+  return(
+    <div>
+      <p>{area[idx]}</p>
+      <p>{data && data.text}</p>
+      <button onClick={inc}>+</button>
+      <button onClick={dec}>-</button>
+    </div>
+  );
+}
